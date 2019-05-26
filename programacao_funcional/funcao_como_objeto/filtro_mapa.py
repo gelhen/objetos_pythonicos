a52f8bb56db75a5b3dbda324bd1c11961e3f762f
gelhen <gelhen@gmail.com>
2019-05-25 22:10:57 -0300

    Implementado metodo map

--- programacao_funcional/funcao_como_objeto/filtro_mapa.py
+++ programacao_funcional/funcao_como_objeto/filtro_mapa.py
@@ -1,9 +1,21 @@
 alunos=[('Renzo', 0), ('Luciano', 10), ('Ada', 9)]
 
+print ('filter com list comprehension')
 print([(nome, nota) for nome, nota in alunos if nota > 5])
+print ('map com list comprehension')
+print([nome for nome, nota in alunos if nota > 5])
+
 
 def possui_nota_maior_que_5(aluno):
     _, nota = aluno
     return nota > 5
-
+print('filter')
 print(list(filter(possui_nota_maior_que_5, alunos)))
+
+
+def extrair_nome(aluno):
+    nome, nota = aluno
+    return nome
+
+print('map')
+print(list(map(extrair_nome, filter(possui_nota_maior_que_5, alunos))))
