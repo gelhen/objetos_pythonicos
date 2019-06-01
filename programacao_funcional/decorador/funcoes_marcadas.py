5e7e85fd95b8dc63141b4e8c60660363f012e7e3
gelhen <gelhen@gmail.com>
2019-05-31 22:56:24 -0300

    Implementado decorator marcador

---
+++ programacao_funcional/decorador/funcoes_marcadas.py
@@ -0,0 +1,16 @@
+from programacao_funcional.decorador.registrador import get_marcadas, marcar
+
+
+def primeira():
+    pass
+
+primeira = marcar(primeira)
+
+@marcar
+def segunda():
+    pass
+
+
+if __name__ == '__main__':
+    for f in get_marcadas():
+        print(f.__name__)
